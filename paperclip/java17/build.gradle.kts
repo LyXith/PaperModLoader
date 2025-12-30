86c859a1880a880bf7f4548b18492d1bf711dd06
wdog5734 <1798643961@qq.com>
2025-12-30 12:45:48 +0800

    init more

---
+++ paperclip/java17/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    java
+    id("io.github.goooler.shadow") version "8.1.7"
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+
+    withSourcesJar()
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.release.set(17)
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.sigpipe:jbsdiff:1.0")
+}
+
+tasks.shadowJar {
+    val prefix = "paperclip.libs"
+    listOf("org.apache", "org.tukaani", "io.sigpipe").forEach { pack ->
+        relocate(pack, "$prefix.$pack")
+    }
+
+    exclude("META-INF/LICENSE.txt")
+    exclude("META-INF/NOTICE.txt")
+}
