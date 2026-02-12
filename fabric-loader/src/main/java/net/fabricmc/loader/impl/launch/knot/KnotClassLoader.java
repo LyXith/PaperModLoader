55736ebfc9b3fdc1cef0cfec40741b0323a6c8c9
LyXith <3938307703@qq.com>
2026-02-12 19:46:43 +0800

    Update KnotClassLoader.java

--- fabric-loader/src/main/java/net/fabricmc/loader/impl/launch/knot/KnotClassLoader.java
+++ fabric-loader/src/main/java/net/fabricmc/loader/impl/launch/knot/KnotClassLoader.java
@@ -116,6 +116,21 @@ final class KnotClassLoader extends AbstractSecureClassLoader implements ClassLo
 
 	@Override
 	protected Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
+		if (name.startsWith("io.papermc.paper.") ||
+        name.startsWith("io.papermc.asm.") ||
+        name.startsWith("com.destroystokyo.paper.") ||
+        name.startsWith("org.bukkit.craftbukkit.") ||
+        name.startsWith("org.spigotmc.")) {
+        try {
+            Class<?> c = originalLoader.loadClass(name);
+            if (resolve) {
+                resolveClass(c);
+            }
+            return c;
+        } catch (ClassNotFoundException e) {
+            // fall through
+        }
+    }
 		return delegate.loadClass(name, resolve);
 	}
 
